9818a335e332bd8bdd57a9124cb57f3c7a58aec3
Francisco Iocco <franciscoiocco6@gmail.com>
2022-12-22 19:40:24 -0300

    section-1 created (presentation)

---
+++ src/pages/Login/index.js
@@ -0,0 +1,15 @@
+import "./index.css";
+
+export default function Login() {
+  return (
+    <>
+      <div className="section-1">
+        <div>
+          <h1>Heritage Handler App</h1>
+          <h2>Quick, smart, easy to use... All in one place!</h2>
+        </div>
+      </div>
+      <div className="section-2"></div>
+    </>
+  )
+}
